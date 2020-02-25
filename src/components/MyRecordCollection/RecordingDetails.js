70bc44fd4d7d617abb18fc225d8fb7fae607ebc0
latoyaallen <me@tailorlabs.io>
2020-02-24 22:10:38 -0500

    Make RecordingDetails responsive

--- src/components/MyRecordCollection/RecordingDetails.js
+++ src/components/MyRecordCollection/RecordingDetails.js
@@ -1,4 +1,50 @@
 import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import {
+  MOBILE_PORTRAIT,
+  MOBILE_LANDSCAPE,
+  TABLET_PORTRAIT,
+  TABLET_LANDSCAPE,
+  LAPTOPS,
+} from '../../lib/breakpoints';
+
+const styles = StyleSheet.create({
+  RecordingDetails__container: {
+    paddingTop: '11%',
+    paddingBottom: '11%',
+    fontSize: '7.4em',
+    color: 'white',
+    textAlign: 'center',
+    margin: 0,
+    [MOBILE_PORTRAIT]: {
+    fontSize: '3.4em',
+    },
+    [MOBILE_LANDSCAPE]: {
+    fontSize: '5.4em',
+    },
+    [TABLET_PORTRAIT]: {
+    fontSize: '7.4em',
+    },
+    [TABLET_LANDSCAPE]: {
+    fontSize: '7.4em',
+    },
+    [LAPTOPS]: {
+    fontSize: '7.4em',
+    }
+  },
+  RecordingDetails__text: {
+    [MOBILE_PORTRAIT]: {
+    },
+    [MOBILE_LANDSCAPE]: {
+    },
+    [TABLET_PORTRAIT]: {
+    },
+    [TABLET_LANDSCAPE]: {
+    },
+    [LAPTOPS]: {
+    }
+  },
+});
 
 const RecordingDetails__container = {
   paddingTop: '11%',
@@ -10,7 +56,7 @@ const RecordingDetails__container = {
 }
 
 const RecordingDetails = (props) => (
-  <div style={RecordingDetails__container}>
+  <div className={css(styles.RecordingDetails__container)}>
     <p>{props.location}</p>
     <p>{props.day}</p>
     <p>{props.year}</p>
